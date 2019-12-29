c78bae6ae5c68f206719c65a832bf163415d6039
Galdino <emersongaldino@hotmail.com>
2019-12-29 20:13:59 -0300

    central message universal

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 import express from "express";
-
+import message from "./config/message";
 const routes = express.Router();
 
 routes.get("/users", async (req, res) => {
   const users = ["Galdino", "Emerson", "Geisa"];
-  return res.json({ Success: true, object: users });
+  return res.json({
+    Success: true,
+    object: users,
+    auth: message.USER_ATUTHENTICATE
+  });
+});
+
+routes.get("/", async (req, res) => {
+  return res.json({
+    Success: false,
+    auth: message.USER_NOT_ATUTHENTICATION
+  });
 });
 
 export default routes;
